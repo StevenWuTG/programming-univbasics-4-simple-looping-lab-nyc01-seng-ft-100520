81827246c51d90d32970937edfc435d63d0bc58c
Steven wu <stevenwu91@icloud.com>
2020-09-15 20:01:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,8 @@
 # Write your methods here
-counter = 0
-def loop_message_five_times(string)
-  while counter >=5 do
-    puts string
+def loop_message_five_times(message)
+  count = 0 
+  while count < 5 do
+    puts message
+    count += 1 
   end
 end
